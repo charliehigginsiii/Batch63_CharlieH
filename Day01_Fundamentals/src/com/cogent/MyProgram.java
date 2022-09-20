f1b1dac2606d31fa335f84ea7822b5aee9c5d298
charliehigginsiii <charliehiggiii@gmail.com>
2022-09-20 16:55:42 -0400

    Commit

---
+++ Day01_Fundamentals/src/com/cogent/MyProgram.java
@@ -0,0 +1,13 @@
+package com.cogent;
+/*ClassmyProgram {
+  Publicstaticvoidmain(String arg[]);{
+   system.out.Println("My First Java Program");
+      }
+  }
+ */
+ 
+public class MyProgram {
+	public static void main(String arg[]){
+		System.out.println("My First Java Program");
+    }
+}
