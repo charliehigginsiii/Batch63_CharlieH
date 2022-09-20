ef15879a0e0abe17d2e143ed63d0710059d0734e
charliehigginsiii <charliehiggiii@gmail.com>
2022-09-20 16:53:41 -0400

    Commit

---
+++ Activities/src/Activity/MyProgram.java
@@ -0,0 +1,13 @@
+package Activity;
+/*ClassmyProgram {
+  Publicstaticvoidmain(String arg[]);{
+   system.out.Println("My First Java Program");
+      }
+  }
+ */
+ 
+public class MyProgram {
+	public static void main(String arg[]){
+		System.out.println("My First Java Program");
+    }
+}
