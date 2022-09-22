eefe2be081316866c8fc0c790d49cc06bd117391
charliehigginsiii <charliehiggiii@gmail.com>
2022-09-22 16:04:02 -0400

    Commit

---
+++ JavaSe_Day04_App1/src/com/app/SwitchExample.java
@@ -0,0 +1,23 @@
+package com.app;
+
+public class SwitchExample {
+	public static void main(String[]args) {
+		int a=0;
+		switch(a) {
+		case 0:
+			System.out.println("0zero");
+			break;
+		case 1:
+			System.out.println("1one");
+			break;
+		case 2:
+			System.out.println("2two");
+			break;
+		case 3:
+			System.out.println("3three");
+			break;
+		default:
+			System.out.println("default");
+		}
+	}
+}
