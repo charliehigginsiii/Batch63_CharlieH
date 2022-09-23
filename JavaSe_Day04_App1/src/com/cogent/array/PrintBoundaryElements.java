249ced9854ad5dc38633bfc1562524c99ce57aa8
charliehigginsiii <charliehiggiii@gmail.com>
2022-09-22 23:34:03 -0400

    Commit

--- JavaSe_Day04_App1/src/com/cogent/array/PrintBoundaryElements.java
+++ JavaSe_Day04_App1/src/com/cogent/array/PrintBoundaryElements.java
@@ -1,5 +1,12 @@
 package com.cogent.array;
 
 public class PrintBoundaryElements {
-
+	public int[][] getBoundryElements(int[][]arr){
+		int[][]boundaryelements=new int[2][8];
+		
+		return boundaryelements;
+	}
+	public static void main(String[]args) {
+		int[][]arr= {{3,5,1},{6,2,4},{9,7,8}};
+	}
 }
